bbac40d09aaaf67a6b36f5a6e452699ccfcdba9b
Alfreadrofan <tibinyesudas@gmail.com>
2022-11-23 23:27:20 +0530

    first commit

--- www/html/Admin/admin_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); 
-if(isset($_GET['id'])){
-$_SESSION["adminid"]=="";
-unset($_SESSION["adminid"]); 	// remove  session variables/
-header('Location:../admin/');
-}
-mysqli_close($con);
-?>
--- www/html/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Create connection
-$con = mysqli_connect( "localhost", "root", "", "artwork" );
-// Check connection
-if ( !$con ) {
-	die( "Connection failed: " . mysqli_connect_error() );
-}
-?>
